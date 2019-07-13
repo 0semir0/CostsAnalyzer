e3f3aa92ed2549a91d5536711221f20b48fd4c91
canbolat.semir <s.canbolat97@outlook.de>
2019-07-13 15:16:09 +0200

    username button function

--- Mitarbeiter_Ausgaben/SignUpWindow.cs
+++ Mitarbeiter_Ausgaben/SignUpWindow.cs
@@ -12,9 +12,52 @@ namespace Mitarbeiter_Ausgaben
 {
     public partial class SignUpWindow : Form
     {
+        private string userName;
+        private string firstName;
+        private string lastName;
+        private string mailAdress;
+        private string password;
+        private string repeatPassword;
+
         public SignUpWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void setInputs()
+        {
+            userName = textBox1.Text;
+            firstName = textBox2.Text;
+            lastName = textBox3.Text;
+            mailAdress = textBox4.Text;
+            password = textBox5.Text;
+            repeatPassword = textBox6.Text;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            userName = "'" + userName + "'";
+
+            FinalWindow fw = new FinalWindow();
+
+            string unameCheck = fw.getCMD($@"SELECT COUNT(*)
+                                             FROM users
+                                             WHERE userName LIKE {userName};").ExecuteScalar().ToString();
+
+            int unameLineCounter = Int32.Parse(unameCheck);
+
+            if(unameLineCounter > 0)
+            {
+                MessageBox.Show("Username is taken, try another one.");
+            }
+
+            else if(unameLineCounter == 0)
+            {
+
+            }
+
         }
     }
 }
